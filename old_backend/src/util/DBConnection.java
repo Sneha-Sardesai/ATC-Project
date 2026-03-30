4f296be0995f33ee9cbc8a708fe60dc05359233e
Sneha-Sardesai <sneha.sardesai86@gmail.com>
2026-03-30 15:08:29 +0530

    backend gaya

--- old_backend/src/util/DBConnection.java
+++ old_backend/src/util/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class DBConnection {
     public static Connection getConnection() throws Exception {
         String url = "jdbc:mysql://localhost:3306/ATC_DB";
-        String user = "atc_user";
-        String password = "atc123";
+        String user = "root";
+        String password = "OpenEveryDoor2386*";
 
         return DriverManager.getConnection(url, user, password);
     }
